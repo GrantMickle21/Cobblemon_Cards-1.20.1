4fd516376f653e31eee7964938bd5043a9538ac2
GrantMickle21 <grant.mickle21@gmail.com>
2024-08-14 13:45:04 -0600

    First commit

---
+++ src/main/java/net/tacosocks/cobblemoncards/CobblemonCardsClient.java
@@ -0,0 +1,10 @@
+package net.tacosocks.cobblemoncards;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CobblemonCardsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
